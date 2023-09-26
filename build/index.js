688f6b81eefcda1d3f73ebcc9fcdd78f874e8ef9
Brenda Brito <bbritob8@gmail.com>
2023-09-25 22:04:35 -0300

    Entendendo configuração do script typescript

--- build/index.js
+++ build/index.js
@@ -1,19 +1,10 @@
+"use strict";
 //pra traduzir o arquivo typescript -> npx tsc (caminho do arquivo typescript) -> ex: src/index.ts
 //pra rodar -> node (caminho do arquivo ts traduzido (js)) -> src/index.js
-
-type hero = {
-    name: string;
-    vulgo: string;
-};
-
-function printaObjeto(pessoa : hero)
-{
+function printaObjeto(pessoa) {
     console.log();
 }
-
 printaObjeto({
     name: "Clark Kent",
     vulgo: "Supermen",
 });
-
-
